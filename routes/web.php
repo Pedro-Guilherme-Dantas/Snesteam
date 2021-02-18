5509550c272e1220d80bc26765b54053a689157c
Lucas Almeida <lucasalmeidda07@gmail.com>
2021-02-18 12:07:14 -0200

    teste-development

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::get('/admin/games',[GameController::class,'index'])->name('admin.games.index');
 Route::get('admin/games/create',[GameController::class,'create'])->name('admin.games.create');
 Route::post('admin/games/store',[GameController::class,'store'])->name('admin.games.store');
+
+Route::get('/teste', function () {
+    return view('welcome');
+});
