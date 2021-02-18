2584d94c2dc71c280fdd3623290e4f9b0ec02a4c
Pablo Gustavo <34045117+Pablo1Gustavo@users.noreply.github.com>
2021-02-18 18:32:01 -0300

    Migrations para o banco de dados e alteraçao no model User para nao gerar os timestamps

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
